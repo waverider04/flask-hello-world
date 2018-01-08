0cb47aabddd34ebc8b684cc30623a417a47b8534
waverider <waverider04@gmail.com>
2018-01-07 21:02:30 -0700

    First commit of my first web app.

---
+++ app.py
@@ -0,0 +1,55 @@
+# ---- Flask Hello World ---- #
+
+# import the Flask class from the Flask package
+from flask import Flask
+
+# create the application object
+app = Flask(__name__)
+
+# error handling
+app.config["DEBUG"] = True
+
+# use the decorator pattern to
+# link the view function to a url
+@app.route("/")
+@app.route("/hello")
+# @app.route("/goodbye")
+
+# define the view using a function that returns a string
+def hello_world():
+	return "Hello, World!?!?!?!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return (search_query)#, status, headers)
+
+# @app.route("/integer/<int:value>")
+# def int_type(value):
+# 	print(value + 1)
+# 	return "correct"
+
+# @app.route("/float/<float:value>")
+# def float_type(value):
+# 	print(value + 1)
+# 	return "correct"
+
+# # dynamic route that accepts slashes
+# @app.route("/path/<path:value>")
+# def path_type(value):
+# 	print(value)
+# 	return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael" :
+		return "Hello, {}".format(name)
+	else:
+		return "Not Found", 404
+
+
+
+
+# start the development server using the run() method
+if __name__ == "__main__":
+	app.run()
